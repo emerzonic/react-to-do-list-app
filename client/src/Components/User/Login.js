5d2279b4325a5b56e760d0f07ea901812b82fc78
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 18:56:00 -0500

    add login logic

---
+++ client/src/Components/User/Login.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+    Component
+} from 'react';
+import { Link } from "react-router-dom";
+// import './home.css';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+    
+    this.handleSubmit = e =>{
+        e.preventDefault();
+        let preciousUser = {
+            username: e.target.username.value,
+            password: e.target.password.value
+        };;
+        if(!preciousUser.username.length ||!preciousUser.password.length){ 
+            return;
+        }else{
+            this.props.handleUserSignin(preciousUser);
+        }
+      }
+}
+
+render() {
+    return (
+        <form id="signinForm" className="ui form" onSubmit={this.handleSubmit}>
+        <h3>Login</h3>
+        <div className="ui divider"></div>
+        <div className="required field">
+        <label>Username</label>
+            <div className="ui left icon input"> <i className="user icon"></i>
+            <input name="username" placeholder="Username" type="text" autoComplete="off"/>
+            </div>
+        </div>
+          <div className="required field">
+            <label>Password</label>
+            <div className="ui left icon input"><i className="lock icon"></i>
+          <input type="password" name="password" placeholder="Password"/>
+        </div>
+          </div>
+        <button type="submit" className="ui orange button">Login</button>
+        <span>Don't have an account? </span>
+        <Link  className="ui item" to={"/signup"}>Sign Up</Link>
+        <div className="ui error message"></div>
+      </form>
+
+    );
+}
+}
+ 
+export default Login;
