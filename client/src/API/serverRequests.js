1cdeaf0737aaa5d1d5ad90d071420b8ac65be7a0
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 18:53:32 -0500

    Add server api request routes

---
+++ client/src/API/serverRequests.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+// import formatter from './formatter';
+
+
+export default {
+  // Send a new user data to the database
+  sendNewUserData: function (newUser) {
+    return axios.post('/signup', newUser);
+  },
+
+  // Send a user login data to the database
+  sendLoginData: function (user) {
+    return axios.post('/signin', user);
+  },
+
+  // Gets all todos
+  getAllTodos: function (userId) {
+    return axios.get("/todos/" + userId);
+  },
+
+  // Gets one todo
+  //  getOneTodo: function (todoId, userId) {
+  //   return axios.get("/todos/"+ todoId,  userId);
+  // },
+
+  // Saves an todos to the database
+  addNewTodos: function (userId, todo) {
+    console.log(todo)
+    return axios.post('/todos/new/' + userId, todo);
+  },
+
+  // Deletes the todos with the given id
+  deleteTodos: function (todoId) {
+    return axios.delete("/" + todoId);
+  }
+};
