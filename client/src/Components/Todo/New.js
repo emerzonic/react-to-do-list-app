c1603a78d799bdc7b872a61931212d0ff439b809
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 18:54:53 -0500

    add todo sub mission form

---
+++ client/src/Components/Todo/New.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Component } from 'react';
+// import './save.css';
+// import Moment from 'moment';
+
+
+class TodoForm extends Component {
+  constructor(props) {
+      super(props); 
+    }
+      render() { 
+        // console.log(this.props)
+        return ( 
+            <form className="ui form" onSubmit={this.props.handleSubmit}>
+                <div className="field">
+                    <div className="ui fluid action input">
+                            <input onChange={this.props.hanldeOnChange} type="text" name="todoText" placeholder="Add new task to todo list" 
+                                  />
+                        <button className="ui button" type="submit"><i className="plus icon"></i></button>
+                    </div>   
+                </div> 
+            </form>  
+            )
+        }
+}
+
+export default TodoForm;
