fcb1c50d5eb626d49108dd8c34b3f3588497b65c
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 18:58:02 -0500

    add model

--- models/Todo.js
+++ models/Todo.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 
-var todoSchema = new mongoose.Schema({
+var TodoSchema = new mongoose.Schema({
   'title': { type: String },
   'notes': { type: String },
   'startDate': { type: Date},
   'dueDate': { type: Date},
-  'status': { type: Boolean, default: false },
+  'status': { type: String, default: 'Pending' },
   'createdAt': { type: Date, default: Date.now },
   'updatedAt': { type: Date, default: Date.now },
 });
 
 
-module.exports = mongoose.model('Todo', todoSchema);
+module.exports = mongoose.model('Todo', TodoSchema);
