2b91b6b9e44260113f0451b20574aa663ed95057
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 19:25:03 -0500

    refactored middleware

--- client/src/Components/Middleware.js
+++ client/src/Components/Middleware.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Component } from 'react';
 import { Redirect } from 'react-router'
 import '../App.css';
 
 
-
-class  Middleware extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-         }
-    }
-    render() { 
-        return (
+const Middleware = () => (
             <div>
-            {localStorage.getItem('user')?
-            <Redirect to='/todos'/>:<Redirect to='/login'/>
-            }
+                {localStorage.getItem('user')?
+                <Redirect to='/todos'/>:<Redirect to='/login'/>
+                }
             </div>
          );
-    }
-}
- 
+     
 export default Middleware;
