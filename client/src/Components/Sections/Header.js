33b8eb0373dd804e025ee9f004f123391ab00dda
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 20:30:23 -0500

    refactor header

--- client/src/Components/Sections/Header.js
+++ client/src/Components/Sections/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import Moment from 'moment';
+// import Moment from 'moment';
 
 const Header = () => (
     <div className="ui segments">
