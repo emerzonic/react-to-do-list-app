b97dc5ebed20ab67d7bb811cafe4f268c9602053
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 19:31:57 -0500

    clean up

--- client/src/Components/Todo/New.js
+++ client/src/Components/Todo/New.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { Component } from 'react';
-// import './save.css';
-// import Moment from 'moment';
 
-
-class TodoForm extends Component {
-      render() { 
-        // console.log(this.props)
-        return ( 
-            <form className="ui form" onSubmit={this.props.handleSubmit}>
+const TodoForm = (props) => (
+            <form className="ui form" onSubmit={props.handleSubmit}>
                 <div className="field">
                     <div className="ui fluid action input">
-                            <input onChange={this.props.hanldeOnChange} type="text" name="todoText" placeholder="Add new task to todo list" 
+                            <input onChange={props.hanldeOnChange} type="text" name="todoText" placeholder="Add new task to todo list" 
                                   />
                         <button className="ui button" type="submit"><i className="plus icon"></i></button>
                     </div>   
                 </div> 
             </form>  
             )
-        }
-}
 
 export default TodoForm;
