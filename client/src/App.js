f32b61f6047f47f80c268021a6dace95d881658b
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 19:24:32 -0500

    refactored router

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Middleware from "./Components/Middleware";
 import User from "./Components/User/Users";
-import Todos from "./Components/Todo/Todos";
+import Todo from "./Components/Todo/Todo";
 import Header from "../src/Components/Sections/Header";
 
 
@@ -11,7 +11,7 @@ const App = () => (
       <div className='ui raised very padded text container segment'>
           <Header/>
           <Route exact path="/" component={Middleware} />
-          <Route exact path="/todos" component={Todos}/>
+          <Route exact path="/todos" component={Todo}/>
           <Route exact path="/login" component={User}/>
           <Route exact path="/signup" component={User}/>
           <Route exact path="/signout" component={User}/>
