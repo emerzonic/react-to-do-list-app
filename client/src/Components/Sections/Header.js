24da3988eff132eecd60b3dfe11bb0f702f17d9d
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 20:30:05 -0500

    refactor header

--- client/src/Components/Sections/Header.js
+++ client/src/Components/Sections/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-// import './home.css';
+import Moment from 'moment';
 
 const Header = () => (
     <div className="ui segments">
@@ -8,10 +8,10 @@ const Header = () => (
         <h1>TODO LIST APP</h1>
         </div>
         <div className="ui secondary segment">
-        <p>Never miss out on your important tasks</p>
+        <p>Never miss out on your important tasks.</p>
         {localStorage.getItem('user')?
         <div className="right floated content">
-            <Link to={"/signout"} className="ui green button">Sign Out</Link>
+            <Link to={"/signout"} className="ui teal basic button">Sign Out</Link>
         </div>
         :""}
         </div>
