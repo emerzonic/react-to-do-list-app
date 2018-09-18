9ac240d4ae9346f48dd27dace529b586bdeb224c
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-17 19:26:26 -0500

    refoactor todo list component

---
+++ client/src/Components/Todo/TodoList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import Moment from 'moment';
+
+
+const TodoList = (props) => (
+            <div className="ui middle aligned divided list">
+                {props.todos.length > 0 ? props.todos.map((todo, i) =>
+                    <div className="item todo-item" key={todo._id}>
+                    <div className="left floated content delete-button">
+                        <div className="ui red icon button delete-button">
+                        <i className="trash icon" 
+                            data-id={todo._id} 
+                            onClick={props.handleDelete} ></i>
+                        </div>
+                    </div>
+                    <div className="content">
+                      <span>{todo.title}</span>
+                    </div>
+                  </div>
+                ):""}
+                </div>    
+            )
+
+
+export default TodoList;
+ 
