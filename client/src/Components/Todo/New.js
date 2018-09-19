8646c87c0dfa1c6e0e259c6b5bb658e5bc6a20e3
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-18 23:33:56 -0500

    add error text

--- client/src/Components/Todo/New.js
+++ client/src/Components/Todo/New.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const TodoForm = (props) => (
             <form className="ui form" onSubmit={props.handleSubmit}>
+            <span className={props.error?'show':'hide'}>Todo must be more than 3 characters.</span>
                 <div className="field">
                     <div className="ui fluid action input">
                             <input onChange={props.hanldeOnChange} type="text" name="todoText" placeholder="Add new task to todo list" 
                                   />
                         <button className="ui button" type="submit"><i className="plus icon"></i></button>
+                        
                     </div>   
                 </div> 
             </form>  
