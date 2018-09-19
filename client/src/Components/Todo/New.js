d7a11c62b34345191c83dba71b8fd9acf4315adf
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-18 23:55:51 -0500

    error message

--- client/src/Components/Todo/New.js
+++ client/src/Components/Todo/New.js
@@ -5,8 +5,10 @@ const TodoForm = (props) => (
             <span className={props.error?'show':'hide'}>Todo must be at least 3 characters.</span>
                 <div className="field">
                     <div className={props.error?"ui fluid action input error":"ui fluid action input"}>
-                            <input onChange={props.hanldeOnChange} type="text" name="todoText" placeholder="Add new task to todo list" 
-                                  />
+                            <input 
+                                onChange={props.hanldeOnChange} type="text" name="todoText" 
+                                placeholder="Add new task to todo list"
+                            />
                         <button className="ui button" type="submit"><i className="plus icon"></i></button>
                         
                     </div>   
