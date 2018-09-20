d8bd4c0fbd88a3a78eefa6f6583b27d680257bb9
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-19 22:30:57 -0500

    working on error display

--- client/src/Components/User/Users.js
+++ client/src/Components/User/Users.js
@@ -7,49 +7,30 @@ import Login from "./Login";
 import SignUp from './SignUp';
 import Signout from './LogOut';
 
-
 class User extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: {
-                _id: '',
-                username: ''
-            }
+            loginError: false,
+            signupError: false
         };
 
-        //This method handle user sign up
+        //This method handles user sign up
         this.handleUserSignup = (newUser) => {
             API.sendNewUserData(newUser).then(res => {
                 if (res.data.username) {
-                    localStorage.setItem('user', res.data.username)
-                    localStorage.setItem('todo_app_user_id', res.data._id)
-                    this.setState({
-                        user: {
-                            id: res.data._id,
-                            username: localStorage.getItem('user')
-                        }
-                    })
-                    this.props.history.push('/todos');
+                    this.storeUserData(res)
                 } else {
                     this.props.history.push('/signup');
                 }
             }).catch(err => console.log(err));
         }
 
-        //This method handle user signin
+        //This method handles user signin
         this.handleUserSignin = (user) => {
             API.sendLoginData(user).then(res => {
                 if (res.data.username) {
-                    localStorage.setItem('user', res.data.username)
-                    localStorage.setItem('todo_app_user_id', res.data._id)
-                    this.setState({
-                        user: {
-                            id: res.data._id,
-                            username: localStorage.getItem('user')
-                        }
-                    })
-                    this.props.history.push('/todos');
+                    this.storeUserData(res)
                 } else {
                     this.props.history.push('/login');
                 }
@@ -57,25 +38,31 @@ class User extends Component {
         }
 
         //This method handles user signout
-        this.handleUserSignout= (action) => {
-            if(action === "positive"){
-            localStorage.removeItem('todo_app_user_id');
-            localStorage.removeItem('user');
-            localStorage.removeItem('todos');
-            this.props.history.push('/');
-            }else{
-            this.props.history.goBack();
+        this.handleUserSignout = (action) => {
+            if (action === "positive") {
+                localStorage.removeItem('todo_app_user_id');
+                localStorage.removeItem('user');
+                this.props.history.push('/');
+            } else {
+                this.props.history.goBack();
             }
+        }
+
+        this.storeUserData = (res) => {
+            localStorage.setItem('user', res.data.username)
+            localStorage.setItem('todo_app_user_id', res.data._id)
+            this.props.history.push('/todos');
         }
     }
     render() {
         return (
             <div>
                 {this.props.match.path === '/signup'?
-                    <SignUp handleUserSignup={this.handleUserSignup}/>:
+                    <SignUp handleUserSignup={this.handleUserSignup} {...this.props}/>:
                     this.props.match.path === '/signout'? 
                     <Signout handleUserSignout={this.handleUserSignout}/>:
-                    <Login handleUserSignin={this.handleUserSignin}/>} 
+                    <Login handleUserSignin={this.handleUserSignin} {...this.props}/>
+                } 
             </div>
         );
     }
