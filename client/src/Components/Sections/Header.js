0027d66e9f1d9232f8015bc1a524a16918209c12
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-19 22:27:42 -0500

    Rename to just todo list

--- client/src/Components/Sections/Header.js
+++ client/src/Components/Sections/Header.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Header = () => (
     <div className="ui segments">
         <div className="ui green segment">
-        <h1>TODO LIST APP</h1>
+        <h1>TODO LIST</h1>
         </div>
         <div className="ui secondary segment">
         <p>Never miss out on your important tasks.</p>
         {localStorage.getItem('user')?
         <div className="right floated content">
-            <Link to={"/signout"} className="ui teal basic button">Sign Out</Link>
+            <Link to={"/signout"} className="ui fluid teal basic button">Sign Out</Link>
         </div>
         :""}
         </div>
